7b56c847907d5bd24b43d080a7662d09612ebf26
Paul AMAR <aos.paul@gmail.com>
2014-03-15 18:50:02 +0100

    Added HostDiscovery module

---
+++ modules/DiscoveryModule/HostDiscoveryModule.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, './modules/UtilsModule')
+from NmapHelper import *
+
+
+class HostDiscoveryModule(NmapHelper):
+
+    def __init__(self, range_ip):
+        self.range_ip = range_ip
+
+    def execute(self):
+        command = " -sP " + self.range_ip + " -oG - "
+        command = command + "| awk '/Up/{print $2}'"
+        res = super(HostDiscoveryModule, self).execute(command, True)
+        # split the string with new line and delete the last one
+        print res
---
+++ modules/DiscoveryModule/HostDiscoveryModuleExample.py
@@ -0,0 +1,4 @@
+from HostDiscoveryModule import *
+
+test = HostDiscoveryModule('192.168.0.0/24')
+test.execute()
