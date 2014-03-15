50f5e8c3e72a33ecf90da0e98819c42dcc77e050
Adrien Hamraoui <dev@hamadr.fr>
2014-03-15 18:27:18 +0100

    HydraModule : first draft
    Hydra modules perform a hydra for a protocol and a list of targets
    It returns a list of dictionaries containing the info to connect to the hosts

---
+++ modules/OffensiveModule/HydraModule.py
@@ -0,0 +1,34 @@
+import subprocess
+import re
+
+class Target:
+    def __init__(self, ip, port):
+        self.ip = str(ip)
+        self.port = str(port)
+
+''' Module example
+targets = [Target("192.168.0.11", "22")]
+module = HydraModule()
+print module.execute("ssh", targets)
+'''
+class HydraModule:
+
+    def __init__(self, hydra_path = "hydra", login_dict = "/tmp/users.txt", pass_dict = "/tmp/pass.txt"):
+        self.hydra_path = hydra_path
+        self.login_dict = login_dict
+        self.pass_dict = pass_dict
+
+    def execute(self, protocol, targets):
+        res = list()
+        for target in targets:
+            args = [self.hydra_path, target.ip, protocol, "-s", target.port, "-P", self.pass_dict, "-L", self.login_dict, "-e", "ns", "-t", "10"]
+            print args
+
+            hydra = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = hydra.communicate()
+
+            pattern = r"login: ([\w]+) \s+password: ([\w]+)"
+            logins = re.findall(pattern, out)
+            for login in logins:
+                res.append({"protocol":protocol, "ip":target.ip, "port":target.port, "user":login[0], "pass":login[1]})
+        return res
