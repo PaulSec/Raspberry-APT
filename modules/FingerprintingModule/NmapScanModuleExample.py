125e0109aef8445053e4de4a5f46564dcf3628ac
Paul AMAR <aos.paul@gmail.com>
2014-03-16 19:45:42 +0100

    returning list only splitted

--- modules/FingerprintingModule/NmapScanModuleExample.py
+++
@@ -1,4 +0,0 @@
-from NmapScanModule import *
-
-test = NmapScanModule('192.168.0.0/24', '22')
-test.execute()
--- modules/UtilsModule/NmapHelper.py
+++ modules/UtilsModule/NmapHelper.py
@@ -1,20 +1,19 @@
 import os
-
+import sys
+sys.path.insert(0, '../../common/')
 from Host import *
 
-nmap_location = "nmap"
+nmap_location = "~/Perso/Hacking/tools/nmap/nmap"
 
 
 class NmapHelper(object):
 
     def execute(self, command, grepable=False):
         command = nmap_location + command
-        out = os.popen(command).read()
+        print command
+        res = os.popen(command).read()
         # split the string with new line and delete the last one
-        res = []
         if (grepable):
-            out = out.split('\n')
-            out = out[:-1]
-            for ip in out:
-                res.append(Host(ip))
+            res = res.split('\n')
+            res = res[:-1]
         return res
