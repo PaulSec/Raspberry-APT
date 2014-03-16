bc08ee400c3312b0f8575eb3be2d7f59d9ff6981
Adrien Hamraoui <dev@hamadr.fr>
2014-03-16 12:44:13 +0100

    Abstract class DiscoveryModule

---
+++ modules/DiscoveryModule/DiscoveryModule.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta, abstractmethod
+
+
+class DiscoveryModule(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.ifs = []
+        self.hosts = []
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+    def getIfs(self):
+        return self.ifs
+
+    def addHost(self, host):
+        self.hosts.append(host)
+
+    def addHosts(self, hosts):
+        self.hosts.extend(hosts)
+
+    def getResults(self):
+        return self.hosts
+
+    def feed(self, ifs):
+        self.ifs.extend(ifs)
