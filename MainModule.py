b9e72dd2b901d595eaa64ed99b1663ef7c18ddfb
Adrien Hamraoui <dev@hamadr.fr>
2014-03-16 20:06:54 +0100

    MainModule load workflows from a config file

--- MainModule.py
+++ MainModule.py
@@ -1,4 +1,8 @@
 import sys
+import importlib
+import os
+import re
+
 sys.path.insert(0, './modules/DiscoveryModule')
 sys.path.insert(0, './modules/FingerprintingModule')
 sys.path.insert(0, './modules/MaintenanceModule')
@@ -7,9 +11,68 @@ sys.path.insert(0, './modules/UtilsModule')
 sys.path.insert(0, './common/')
 
 from Interface import *
-from HostDiscoveryModule import *
+from Host import *
+from Target import *
 from InterfacesRetriever import *
 from InternetChecker import *
+
+def find_file(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+def load_module(module_name):
+    file_path = find_file(module_name + ".py", "modules")
+    if file_path != None:
+        module = importlib.import_module(module_name)
+        return getattr(module, module_name)
+    print "Cannot find %s.py" % module_name
+    return None
+
+def parse_workflows(file_name):
+    flowname_regexp = r"\[WF=(\w+)\]"
+    flows = {}
+    lastflow = ""
+    for line in open(file_name).read().splitlines():
+        if len(lastflow) == 0:
+            #Parsing flow name
+            regex = re.search(flowname_regexp, line)
+            if regex != None:
+                lastflow = regex.group(1)
+            else:
+                print "Expected begining of a flow, found %s" % line
+                return None
+        else:
+            #Parsing flow
+            modulenames = line.split("->")
+            modules = []
+            for modulename in modulenames:
+                module = load_module(modulename)
+                if module == None:
+                    print "Cannot load module: " + modulename
+                    return None
+                modules.append(module)
+            if len(modules) != 3:
+                print "Expected three modules. See doc for more info."
+                return None
+            flows[lastflow] = modules
+            lastflow = ""
+    return flows
+
+if len(sys.argv) != 2:
+    print "Usage is %s <config file>" % sys.argv[0]
+    sys.exit(0)
+
+cfg_file_name = sys.argv[1]
+if not os.path.isfile(cfg_file_name):
+    print "Cannot find the specified configuration file: %s" % cfg_file_name
+
+flows = parse_workflows(cfg_file_name)
+
+if flows == None:
+    exit(0)
+
+print "[+] Config file loaded, %d flows found." % len(flows)
 
 internet_checker = InternetChecker()
 if (internet_checker.check_internet()):
@@ -18,11 +81,12 @@ if (internet_checker.check_internet()):
 interfaces = InterfacesRetriever().get_up_interfaces()
 if (len(interfaces) > 0):
     print "[+] Seems that there are some up interface(s)"
-#for interface in interfaces:
-#    host_discovery = HostDiscoveryModule(range_ip)
-#    res = host_discovery.execute()
-#    print res
-discovery = HostDiscoveryModule()
-discovery.feed(interfaces)
-discovery.execute()
-hosts = discovery.getResults()
+
+for name, flow in flows.iteritems():
+    print "Executing %s" % name
+    moduleResults = interfaces
+    for module in flow:
+        instance = module()
+        instance.feed(moduleResults)
+        instance.execute()
+        moduleResults = instance.getResults()
